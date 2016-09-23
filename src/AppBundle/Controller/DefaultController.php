abb7e465b21920ae85a310a4ebcdfc18dc9e9318
Yvan Sraka <ysraka@simplon.co>
2016-09-23 19:31:45 +0200

    Update DefaultController.php

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,14 +13,6 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $url = 'http://api.openweathermap.org/data/2.5/weather?q='.$_GET['q'].'&APPID=c4d64189685c30187df7546364d23e5b';
-        $obj = json_decode(file_get_contents($url), true);
-
-        return $this->render('default/index.html.twig', [
-            'wind_speed' => $obj['wind']['speed'],
-            // 'temperature': TODO
-            // 'humidity': TODO
-            // etc..
-        ]);
+        return $this->render('default/index.html.twig', []);
     }
 }
